f6617db4a76c434b93a33e9a1e45f483e8828652
lyndapham <lyndapham@google.com>
2020-07-07 19:40:22 +0000

    Add app engine dependency and add comment text area

--- lyndapham/step/portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ lyndapham/step/portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -18,6 +18,9 @@ import com.google.gson.Gson;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -46,6 +49,7 @@ public class DataServlet extends HttpServlet {
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
     // Get the input from the form.
     String vote = request.getParameter("picture-vote");
+    String comment = request.getParameter("text-input");
 
     pictureVotes.add(vote);
 
