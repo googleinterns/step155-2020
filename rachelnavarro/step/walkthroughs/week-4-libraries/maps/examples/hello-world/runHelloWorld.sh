516337f37e7d13329f0ec5404d0448ca4f83aa93
Rachel Navarro <rachelnavarro@google.com>
2020-07-13 19:08:08 +0000

    address review comments: hide API key from walkthroughs

---
+++ rachelnavarro/step/walkthroughs/week-4-libraries/maps/examples/hello-world/runHelloWorld.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+chmod +x /home/rachelnavarro/proj/step155-2020/rachelnavarro/step/walkthroughs/week-4-libraries/maps/examples/hello-world/APIsub.sh
+/home/rachelnavarro/proj/step155-2020/rachelnavarro/step/walkthroughs/week-4-libraries/maps/examples/hello-world/APIsub.sh
+mvn package appengine:run
