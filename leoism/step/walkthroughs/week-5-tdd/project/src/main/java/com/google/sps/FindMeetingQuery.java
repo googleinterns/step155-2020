8e0d5c4e91daa976154c06adb966977b2b0a41b4
Leoism <contact@leoism.me>
2020-07-17 16:59:44 -0700

    Update FindMeetingQuery.java to pass all starter tests (#55)
    Update FindMeetingQuery.java to pass all starter tests

--- leoism/step/walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ leoism/step/walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -14,10 +14,131 @@
 
 package com.google.sps;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashSet;
 
 public final class FindMeetingQuery {
+  /**
+   * Returns a Collection of TimeRange for the requested meeting can take place and attendees can
+   * attend. Assumes that all events are provided in order from start of day to end of day.
+   */
   public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
-    throw new UnsupportedOperationException("TODO: Implement this method.");
+    ArrayList<TimeRange> slotsOpen = new ArrayList<TimeRange>();
+
+    // A requested meeting duration longer than the time of day should not be possible.
+    if (request.getDuration() > TimeRange.WHOLE_DAY.duration()) {
+      return slotsOpen;
+    }
+
+    if (events.isEmpty()) {
+      slotsOpen.add(TimeRange.WHOLE_DAY);
+      return slotsOpen;
+    }
+
+    ArrayList<TimeRange> slotsTaken = getCombinedSlotsTaken(events, request);
+
+    int previousEndTime = TimeRange.START_OF_DAY;
+    for (TimeRange occupied : slotsTaken) {
+      slotsOpen.add(TimeRange.fromStartEnd(previousEndTime, occupied.start(), false));
+      previousEndTime = occupied.end();
+    }
+
+    slotsOpen.add(TimeRange.fromStartEnd(previousEndTime, TimeRange.END_OF_DAY, true));
+
+    return getAvailableTimes(slotsOpen, request);
+  }
+
+  /**
+   * Checks if the requested time interval overlaps with another. If true, then the conflicting
+   * TimeRange is removed and a new TimeRange is returned with the events time merged. Otherwise,
+   * returns null if no TimeRange overlaps.
+   */
+  private TimeRange combineOverlaps(TimeRange requestedTime, ArrayList<TimeRange> slotsTaken) {
+    for (TimeRange occupied : slotsTaken) {
+      if (requestedTime.overlaps(occupied)) {
+        // If an overlap occurs, merge them by getting the earliest start time and the latest start
+        // time.
+        int startTime =
+            occupied.start() > requestedTime.start() ? requestedTime.start() : occupied.start();
+        int endTime = occupied.end() > requestedTime.end() ? occupied.end() : requestedTime.end();
+        slotsTaken.remove(occupied);
+        return TimeRange.fromStartEnd(startTime, endTime, false);
+      }
+    }
+    return null;
+  }
+
+  /**
+   * Checks if all required attendees (those that the event is booked under) are the same as the one
+   * making the Meeting Request. Returns false if a required attendee is not present. Otherwise,
+   * returns true.
+   */
+  private boolean areRequiredAttendeesPresent(
+      Collection<String> requiredAttendees, Collection<String> presentAttendees) {
+    for (String attendee : requiredAttendees) {
+      if (!presentAttendees.contains(attendee)) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  /**
+   * Stores all attendees which the event is booked under and stores them inside a Set to avoid
+   * duplicates. Returns a HashSet with all attendees booked under an event.
+   */
+  private HashSet<String> allEventAttendees(Collection<Event> events) {
+    HashSet<String> eventAttendees = new HashSet<String>();
+    for (Event event : events) {
+      eventAttendees.addAll(event.getAttendees());
+    }
+
+    return eventAttendees;
+  }
+
+  /**
+   * Returns an ArrayList of TimeRange of all slots that are occupied by events. Assumes that all
+   * events are provided in sorted order from start of day to end of day.
+   */
+  private ArrayList<TimeRange> getCombinedSlotsTaken(
+      Collection<Event> events, MeetingRequest request) {
+    ArrayList<TimeRange> slotsTaken = new ArrayList<TimeRange>();
+    Collection<String> mandatoryAttendees = request.getAttendees();
+    Collection<String> eventAttendees = allEventAttendees(events);
+    boolean mandatoryAttendeesPresent =
+        areRequiredAttendeesPresent(mandatoryAttendees, eventAttendees);
+    if (!mandatoryAttendeesPresent) {
+      return slotsTaken;
+    }
+
+    for (Event event : events) {
+      TimeRange requestedTime = event.getWhen();
+      TimeRange overlappedCombined = combineOverlaps(requestedTime, slotsTaken);
+
+      if (overlappedCombined != null) {
+        slotsTaken.add(overlappedCombined);
+        continue;
+      }
+      slotsTaken.add(requestedTime);
+    }
+
+    return slotsTaken;
+  }
+
+  /**
+   * Checks all available slots to see if the there is sufficient time for the requested meeting
+   * duration. If there is a sufficient time slot, it is added to an ArrayList of TimeRange. When
+   * all slots have been checked, returns an ArrayList of TimeRange with sufficient times.
+   */
+  private ArrayList<TimeRange> getAvailableTimes(
+      ArrayList<TimeRange> slotsOpen, MeetingRequest request) {
+    ArrayList<TimeRange> sufficientTimes = new ArrayList<TimeRange>();
+    for (TimeRange availableSlot : slotsOpen) {
+      if (availableSlot.duration() >= request.getDuration()) {
+        sufficientTimes.add(availableSlot);
+      }
+    }
+    return sufficientTimes;
   }
 }
