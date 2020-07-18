dbf7b485f273b4de59fc675fe377e13ca8384612
Lynda Pham <lyndapham@google.com>
2020-07-17 17:12:23 -0700

    Implement sentiment analysis to hide negative comments (#56)
    * Implement sentiment analysis to hide negative comments
    
    * Add VoteResponse class to maintain json response type and display message about hidden language

--- lyndapham/step/portfolio/src/main/java/com/google/sps/data/Vote.java
+++ lyndapham/step/portfolio/src/main/java/com/google/sps/data/Vote.java
@@ -2,14 +2,22 @@
 
 package com.google.sps.servlets;
 
-/** An item on a todo list. */
+/** A vote for a picture. */
 public final class Vote {
 
   private final String vote;
   private final String comment;
+  private final double sentimentScore;
 
   public Vote(String vote, String comment) {
     this.vote = vote;
     this.comment = comment;
+    sentimentScore = (double) 0.0;
+  }
+
+  public Vote(String vote, String comment, double sentimentScore) {
+    this.vote = vote;
+    this.comment = comment;
+    this.sentimentScore = sentimentScore;
   }
 }
