933b7535c404471a3bb2793641e3cf3dfbfbdb04
lyndapham <lyndapham@google.com>
2020-07-21 04:48:42 +0000

    Implement FindMeetingQuery so that it passes all but notEnoughRoom and justEnoughRoom

--- lyndapham/step/walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ lyndapham/step/walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -14,10 +14,110 @@
 
 package com.google.sps;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.Collections;
 
 public final class FindMeetingQuery {
+  /*
+    This class finds available meeting times given an Event.
+  */
+
+  /**
+   * Param:
+   *
+   * Assumptions: Events are sorted (? is this necessary?)
+   *              All events are passed correctly to MeetingRequest
+   *              All events that attendees are attending are provided
+   *              All attendees are mandatory
+   */
   public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
-    throw new UnsupportedOperationException("TODO: Implement this method.");
+    ArrayList<TimeRange> unavailableTimes = new ArrayList<>();
+    ArrayList<TimeRange> availableTimes = new ArrayList<>();
+
+    if (request.getAttendees().isEmpty()) {
+        return Arrays.asList(TimeRange.WHOLE_DAY);
+    }
+
+    if (request.getDuration() >= TimeRange.WHOLE_DAY.duration()) {
+        return availableTimes;
+    }
+
+    unavailableTimes = getUnavailableTimes(events, request);
+
+    availableTimes = getAvailableTimes(unavailableTimes, request);
+
+    return availableTimes;
+  }
+
+  private ArrayList<TimeRange> getUnavailableTimes(Collection<Event> events, MeetingRequest request) {
+    ArrayList<TimeRange> unavailableTimes = new ArrayList<>();
+
+    for (Event event : events) {
+        if (containsAttendee(event, request)) {
+          unavailableTimes.add(event.getWhen());
+        }
+    }
+    unavailableTimes = collapseUnavailableTimes(unavailableTimes);
+    Collections.sort(unavailableTimes, TimeRange.ORDER_BY_START);
+    return unavailableTimes;
+  }
+
+  private Boolean containsAttendee(Event event, MeetingRequest request) {
+      for (String attendee: event.getAttendees()) {
+          if (request.getAttendees().contains(attendee)) {
+              return true;
+          }
+      }
+      return false;
+  }
+
+  private ArrayList<TimeRange> collapseUnavailableTimes(ArrayList<TimeRange> uncollapsed) {
+    ArrayList<TimeRange> collapsed = new ArrayList<>();
+    if (uncollapsed.size() > 0) {
+      collapsed.add(uncollapsed.get(0));
+      int collapsedIdx = 0; // could also use size() - 1
+      for (int i = 1; i < uncollapsed.size(); i++) {
+        // check if next TimeRange is equal to, is contained in, or overlaps, with collapsed time
+        if (collapsed.get(collapsedIdx).equals(uncollapsed.get(i)) ||
+          collapsed.get(collapsedIdx).contains(uncollapsed.get(i)) ||
+          uncollapsed.get(i).contains(collapsed.get(collapsedIdx))) {
+          continue;
+          // TODO: test if equals check is necessary
+        } else if (collapsed.get(collapsedIdx).overlaps(uncollapsed.get(i))) {
+          // start should be smallest start time, end should be the largest end time
+          int start = (collapsed.get(collapsedIdx).start() < uncollapsed.get(i).start()) ? 
+            collapsed.get(collapsedIdx).start() : uncollapsed.get(i).start();
+          int end = (collapsed.get(collapsedIdx).end() > uncollapsed.get(i).end()) ? 
+            collapsed.get(collapsedIdx).end() : uncollapsed.get(i).end();
+          collapsed.add(TimeRange.fromStartEnd(start, end, false));
+          collapsed.remove(collapsedIdx);
+        } else {
+          // there is no overlap, just add to collapsed as a different entity
+          collapsed.add(uncollapsed.get(i));
+          collapsedIdx++;
+        }
+      }
+    }
+    return collapsed;
+  }
+
+  private ArrayList<TimeRange> getAvailableTimes(ArrayList<TimeRange> unavailableTimes, MeetingRequest request) {
+    ArrayList<TimeRange> availableTimes = new ArrayList<>();
+    int startTime = TimeRange.START_OF_DAY;
+    for (TimeRange time : unavailableTimes) {
+      availableTimes.add(TimeRange.fromStartEnd(startTime, time.start(), false));
+      startTime = time.end();
+    }
+
+    availableTimes.add(TimeRange.fromStartEnd(startTime, TimeRange.END_OF_DAY, true));
+
+    for (TimeRange time : availableTimes) {
+        if (time.duration() < request.getDuration()) {
+            availableTimes.remove(time);
+        }
+    }
+    return availableTimes;
   }
 }
