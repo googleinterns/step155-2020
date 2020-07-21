db5c995cb00f69f3396f3bdaba3bb742a387fc55
Rachel Navarro <rachelnavarro@google.com>
2020-07-21 20:23:51 +0000

    fix lint errors in FindMeetingQuery.java

--- rachelnavarro/step/walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ rachelnavarro/step/walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -14,13 +14,11 @@
 
 package com.google.sps;
 
-import java.util.Collection;
-import java.util.Collections;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.Comparator;
-import java.util.HashSet;
-import java.util.Set;
 
 public final class FindMeetingQuery {
 
@@ -36,13 +34,16 @@ public final class FindMeetingQuery {
     long meetingLength = request.getDuration();
     ArrayList<Event> prescheduledEvents = new ArrayList<Event>(events);
 
-    // A meeting that has a negative duration or that is longer than the whole day cannot be scheduled at all.
+    // A meeting that has a negative duration or that is longer than the whole day cannot be
+    // scheduled at all.
     if (meetingLength < 0 || meetingLength > TimeRange.WHOLE_DAY.duration()) {
       return Arrays.asList();
     }
 
     // Sort the prescheduled events by their start time.
-    Comparator<Event> sortByStart =  (Event eventA, Event eventB) -> Long.compare(eventA.getWhen().start(), eventB.getWhen().start());
+    Comparator<Event> sortByStart =
+        (Event eventA, Event eventB) ->
+            Long.compare(eventA.getWhen().start(), eventB.getWhen().start());
     Collections.sort(prescheduledEvents, sortByStart);
 
     // Determine available times and update possibleTimes based off of findings.
@@ -53,7 +54,8 @@ public final class FindMeetingQuery {
   /*
    * See if a prescheduled event and the requested meeting have an attendee in common.
    */
-  private boolean attendeesInCommon(Collection<String> meetingAttendees, ArrayList<String> eventAttendees) {
+  private boolean attendeesInCommon(
+      Collection<String> meetingAttendees, ArrayList<String> eventAttendees) {
     for (String attendee : eventAttendees) {
       if (meetingAttendees.contains(attendee)) {
         return true;
@@ -82,7 +84,10 @@ public final class FindMeetingQuery {
   /*
    * Update possibleTimes to include any times during which the meeting request could be scheduled.
    */
-  private void getPossibleTimes(ArrayList<Event> prescheduledEvents, Collection<String> meetingAttendees, long meetingLength) {
+  private void getPossibleTimes(
+      ArrayList<Event> prescheduledEvents,
+      Collection<String> meetingAttendees,
+      long meetingLength) {
 
     for (Event event : prescheduledEvents) {
 
@@ -95,7 +100,8 @@ public final class FindMeetingQuery {
           return;
         }
 
-        // If there's enough time between now and the prescheduled event, schedule the requested meeting in-between.
+        // If there's enough time between now and the prescheduled event, schedule the requested
+        // meeting in-between.
         int timeBetween = getTimeBetween(event, currentTime);
         if (timeBetween >= meetingLength) { 
           scheduleMeetingBetween(event, timeBetween);
