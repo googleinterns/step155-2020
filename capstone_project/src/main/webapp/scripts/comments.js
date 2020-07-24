fc4601cd9700807fb5a92fdabfa500c16e894d9d
Leonardo Mota-Villaraldo <leoism@google.com>
2020-07-24 12:09:26 -0700

    Fix linter issues

--- capstone_project/src/main/webapp/scripts/comments.js
+++ capstone_project/src/main/webapp/scripts/comments.js
@@ -1,28 +1,26 @@
-/**
- * 
- */
+/** Adds the user post to the DOM. */
 function postComment() {
   const postText = document.getElementById('post-entry').value.trim();
   const userPosts = document.getElementById('user-posts');
   const imageURL = document.getElementById('image-preview').src;
 
-  let newPost = document.createElement('div');
-  if(imageURL) {
+  const newPost = document.createElement('div');
+  if (imageURL) {
     newPost.innerHTML = `<img src="${imageURL}">`;
   }
-  
+
   newPost.innerHTML += `<p>${postText}</p>`;
 
   userPosts.appendChild(newPost);
-  document.getElementById('image-preview').src = ''
+  document.getElementById('image-preview').src = '';
   document.getElementById('image-upload').value = '';
 }
+
 /**
  * Loads a preview of the image to be uploaded.
- * @param {Event} event  
+ * @param {Event} event current state of the image tag
  */
 function previewImage(event) {
-  console.log(typeof event);
   const preview = document.getElementById('image-preview');
   preview.src = URL.createObjectURL(event.target.files[0]);
 }
