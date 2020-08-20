79ecab8b2b2664f30c0273c84d35d80352c294b6
Leonardo Mota-Villaraldo <leoism@google.com>
2020-08-20 16:55:27 -0700

    fix linter errors

--- capstone_project/src/main/webapp/scripts/comments.js
+++ capstone_project/src/main/webapp/scripts/comments.js
@@ -239,7 +239,7 @@ function disableInput(event) { // eslint-disable-line no-unused-vars
  * @param {HTMLDivElement} element - the requested reaction to send.
  */
 async function reactToPost(element) { // eslint-disable-line no-unused-vars
-  const postID = element.closest(".post-container[data-id]").dataset.id;
+  const postID = element.closest('.post-container[data-id]').dataset.id;
   const reaction = element.dataset.reaction;
   if (reaction === undefined || reaction === '') {
     return;
