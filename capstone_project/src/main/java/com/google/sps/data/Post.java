38e407fbe0e4631ffbcd195c832934edaa400de0
Leonardo Mota-Villaraldo <leoism@google.com>
2020-09-01 08:58:11 -0700

    Add comments to Post class and reorder entity setup

--- capstone_project/src/main/java/com/google/sps/data/Post.java
+++ capstone_project/src/main/java/com/google/sps/data/Post.java
@@ -47,46 +47,58 @@ public class Post {
       return this;
     }
 
+    /** Sets the fileType of the post. */
     public Builder setFileType(String fileType) {
       this.fileType = fileType;
       return this;
     }
 
+    /** Sets the title of the post. */
     public Builder setTitle(String title) {
       this.title = title;
       return this;
     }
 
+    /** Sets the school name of the post. */
     public Builder setSchoolName(String schoolName) {
       this.schoolName = schoolName;
       return this;
     }
 
+    /** Sets the file blob key string of the post. */
     public Builder setFileBlobKey(String fileBlobKey) {
       this.fileBlobKey = fileBlobKey;
       return this;
     }
 
+    /** Sets the reactions of the post in a map. */
     public Builder setReactions(Map<String, Object> reactions) {
       this.reactions = reactions;
       return this;
     }
 
+    /** Sets the amount of upvotes a post has. */
     public Builder setUpvotes(long upvotes) {
       this.upvotes = upvotes;
       return this;
     }
 
+    /** Sets the post id of the post. */
     public Builder setPostId(long postId) {
       this.postId = postId;
       return this;
     }
 
+    /** Returns a Post instance with all the properties of this builder. */
     public Post build() {
       return new Post(this);
     }
   }
 
+  /**
+   * Private constructor that is called from the Builder class. Sets all properties of this Post
+   * instance to the ones from the builder.
+   */
   private Post(Builder builder) {
     this.text = builder.text;
     this.fileType = builder.fileType;
@@ -98,38 +110,47 @@ public class Post {
     this.postId = builder.postId;
   }
 
+  /** Returns a Builder object for the post. */
   public static Builder newBuilder() {
     return new Builder();
   }
 
+  /** Returns the text message of the post. */
   public String getText() {
     return this.text;
   }
 
+  /** Returns the file type of the post. */
   public String getFileType() {
     return this.fileType;
   }
 
+  /** Returns the title of the post. */
   public String getTitle() {
     return this.title;
   }
 
+  /** Returns the school name of the post. */
   public String getSchoolName() {
     return this.schoolName;
   }
 
+  /** Returns the file blob key string of the post. */
   public String getFileBlobKey() {
     return this.fileBlobKey;
   }
 
+  /** Returns a Map of reactions of the post. */
   public Map<String, Object> getReactions() {
     return this.reactions;
   }
 
+  /** Returns the amount of upvotes a post has. */
   public long getUpvotes() {
     return this.upvotes;
   }
 
+  /** Returns the post id of the post. */
   public long getPostId() {
     return this.postId;
   }
