22d601087f0f2d9c7ea65d77b2897eb65fddd210
Lynda <lynda.pham12@gmail.com>
2020-09-03 18:20:16 +0000

    Remove unused import statement

--- capstone_project/src/test/java/com/google/sps/ResourceTest.java
+++ capstone_project/src/test/java/com/google/sps/ResourceTest.java
@@ -19,7 +19,6 @@ import static org.junit.Assert.assertEquals;
 import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.datastore.FetchOptions;
-import com.google.appengine.api.datastore.PreparedQuery;
 import com.google.appengine.api.datastore.Query;
 import com.google.appengine.api.datastore.Query.Filter;
 import com.google.appengine.api.datastore.Query.FilterOperator;
